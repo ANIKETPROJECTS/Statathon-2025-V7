f8db292b379373e12c8d3a79b2ec75bf6e7ac5c1
ANIKETPROJECTS <aniketraneworkemail@gmail.com>
2026-06-17 10:42:49 +0530

    Update environment variables in ecosystem config
    Updated MongoDB URI and session secret for production.

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,8 +5,8 @@ module.exports = {
     env: {
       NODE_ENV: "production",
       PORT: 3009,
-      MONGODB_URI: "PASTE_YOUR_MONGODB_URI_HERE",
-      SESSION_SECRET: "PASTE_YOUR_SESSION_SECRET_HERE"
+      MONGODB_URI: "mongodb+srv://raneaniket23_db_user:F9ydRPZJEZnKBq24@statathon.hgnltng.mongodb.net/?appName=STATATHON",
+      SESSION_SECRET: "f7c1a8e3b5d2c9a4e0f8b1c3d5e2a6f7b3c9d0e1a2b3c4d5e6f7a8b9c0d1e2f3"
     }
   }]
 };
